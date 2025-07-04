d7f9819be2f756d977b385a508d3df41042b7a0d
mahbubshaun <mdmahbubshaun@gmail.com>
2025-07-05 01:32:22 +0600

    aws server

--- src/sections/ChatWidget/ChatWidget.jsx
+++ src/sections/ChatWidget/ChatWidget.jsx
@@ -178,7 +178,7 @@ const handleSubmit = async (e) => {
     setIsTyping(true);
 
     try {
-        const response = await fetch("http://54.146.99.28/chat", {
+        const response = await fetch("https://54.146.99.28:5000/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
