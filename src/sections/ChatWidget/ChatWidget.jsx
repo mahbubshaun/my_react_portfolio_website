6c6feef18a75d73ec5fba0f2b1b0a5a6fd4e7f25
mahbubshaun <mdmahbubshaun@gmail.com>
2025-07-05 18:43:53 +0600

    change url

--- src/sections/ChatWidget/ChatWidget.jsx
+++ src/sections/ChatWidget/ChatWidget.jsx
@@ -178,7 +178,7 @@ const handleSubmit = async (e) => {
     setIsTyping(true);
 
     try {
-        const response = await fetch("https://589d-54-146-99-28.ngrok-free.app/chat", {
+        const response = await fetch("https://e8d7-35-174-6-245.ngrok-free.app/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
